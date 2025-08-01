b5eaa910035aab7c12133cc35f11c0d015982332
Sanders Gutiérrez <ing.sanders@gmail.com>
2025-08-01 02:27:52 -0300

    feat: add DateFormatter component and integrate it into PostPreview

---
+++ src/components/date-formatter.tsx
@@ -0,0 +1,10 @@
+import { format, parseISO } from "date-fns"
+
+type Props = {
+    dateString: string
+}
+
+export function DateFormatter({ dateString } : Props) {
+    const date = parseISO(dateString)
+    return <time dateTime={dateString}>{format(date, "LLLL d, yyy")}</time>
+}
--- src/components/post-preview.tsx
+++ src/components/post-preview.tsx
@@ -2,6 +2,7 @@ import { type Author } from '@/interfaces/author'
 import Link from 'next/link'
 import { Avatar } from './avatar'
 import { CoverImage } from './cover-image'
+import { DateFormatter } from './date-formatter'
 
 type Props = {
     title: string
@@ -19,12 +20,10 @@ export function PostPreview({ title, coverImage, date, excerpt, author, slug }: 
                 <CoverImage title={title} src={coverImage} slug={slug} />
             </div>
             <h3>
-                <Link href={`/blog/${slug}`}>
-                    {title}
-                </Link>
+                <Link href={`/blog/${slug}`}>{title}</Link>
             </h3>
             <div>
-                {date}
+                <DateFormatter dateString={date} />
             </div>
             <p>{excerpt}</p>
             <Avatar name={author.name} picture={author.picture} />
