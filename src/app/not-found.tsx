11ba385e4e7d430ab14973d8315f08cf8a83c562
Sanders Gutiérrez <ing.sanders@gmail.com>
2025-09-08 04:51:59 -0300

    feat(not-found): add custom not-found page
    Signed-off-by: Sanders Gutiérrez <ing.sanders@gmail.com>

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,16 +2,18 @@
 
 import styles from '@/styles/NotFound.module.css'
 import { usePathname } from 'next/navigation'
+import Link from 'next/link'
 
 export default function NotFound() {
     const pathname = usePathname()
 
     return (
-        <section className={`${styles.NotFound}`}>
-            <span className={styles.ErrorCode}>404</span>
-            <p>
+        <section className={styles.NotFound}>
+            <span className={styles.NotFound__code}>404</span>
+            <p className={styles.NotFound__text}>
                 <span>This page <i>{pathname}</i> does not exist</span>
                 <span>Please check the URL in the address bar and try again.</span>
+                <span>Or go back to the <Link href="/" className={styles.NotFound__link}>home</Link></span>
             </p>
         </section>
     )
